f2b114921016d774980fad668a7e34abf8a280a0
shubhamkriitr <shubhamkumar.iitr@gmail.com>
2019-01-24 00:18:41 +0530

    Data creator functions almost ready.
    Next:
    - Test on the workstation
    - Make reuired changes

--- Codes/_x_new_parkinson_data_h5.py
+++ Codes/_x_new_parkinson_data_h5.py
@@ -262,8 +262,8 @@ def _class_from_key(key):
 def create_new_dev_dataset(ip_file_loc, op_file_loc):
     m = load_mat_file(ip_file_loc) # data_dict
     k = get_sorted_array_of_keys(m, condition_on_dev_data_keys)
-    pd_class_mapping = {0:'MSA', 1:'PSP', 2:'PD'}#DO NOT CHANGE
-    class_to_label = {'MSA':0, 'PSP':1, 'PD':2}
+    label_to_name = {0:'MSA', 1:'PSP', 2:'PD'}#DO NOT CHANGE
+    name_to_label = {'MSA':0, 'PSP':1, 'PD':2}
     # chunks: when the keys(MSA_<n>,..,PD_<m>...PSP_.. ) are arranged in ascending order
     # the keys correponding to MSA(label=0), PSP(label=1) and PD(label=2)
     # lie in the following set of closed ranges.
@@ -273,6 +273,10 @@ def create_new_dev_dataset(ip_file_loc, op_file_loc):
 
     summary_path = op_file_loc[:-3]+"_summary.txt"
     summ = open(summary_path,"w")
+    summ.write("\nLabel to name: "+str(label_to_name))
+    summ.write("\nName to label: "+str(name_to_label))
+    summ.write("\nChunks : "+str(chunks))
+    summ.write("Break points"+str(break_points))
     summ.write("\nInput file location: "+ip_file_loc)
     summ.write("\nOutput file location: "+op_file_loc)
     
@@ -287,6 +291,39 @@ def create_new_dev_dataset(ip_file_loc, op_file_loc):
     op.create_dataset("volumes",shape=shape,dtype=np.float32)
     op.create_dataset("labels",shape=(N,1),dtype=np.float32)
     op.create_dataset("one_hot_labels",shape=(N,3,1),dtype=np.float32)
+
+    i = -1 # current index on output dataset
+    for class_id in range(3):
+        class_name = label_to_name[class_id]
+        summ.write("\n"+"-"*10+"c={} -- {}".format(class_id, class_name)+"-"*10)
+        r = chunks[class_name]
+        summ.write("\nWill use chunk = "+str(r))
+        summ.write("\n" + "**"*10)
+        assert(break_points[class_id][0]==(i+1))
+        summ.write("\nStarting at index:"+str(i+1))
+        for u in range(r[0],r[1]+1):# u is current index on key list `k`
+            i+=1
+            key = k[u]
+            assert(_class_from_key(key)==class_name)
+            assert(m[key].shape == (95,79,69))
+            d = (m[key]).reshape(95,79,69,1).astype(np.float32)
+            d = np.transpose(d, [0,2,1,3])
+            assert(d.shape==(95,69,79,1))
+            op["volumes"][i,:,:,:,:] = d
+            op["labels"][i,0] = class_id
+            op["one_hot_labels"][i,class_id,0] = 1.0
+            logstr = "\n"+"OP_idx="+str(i)+" key:"+key+" key_idx:"+str(u)
+            logstr += " ClassId:"+str(class_id)+" Name:"+class_name
+            summ.write(logstr)
+
+        assert(break_points[class_id][1]==i)
+        summ.write("\nEnding at index="+str(i))
+        summ.write("\n"+"**"*10)
+    op.close()
+    summ.close()
+
+
+
 
     for i in range(len(k)):
         key = k[i]
